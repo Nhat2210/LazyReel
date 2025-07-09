6730c9ed67d19d2f880a4a5732456d89b1b2badc
NhatCao <anhnhat22102003@gmail.com>
2025-07-10 00:32:05 +0700

    feat(Admin Interface): Dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,11 @@ import MyBookings from './pages/MyBookings'
 import Favourite from './pages/Favourite'
 import { Toaster } from 'react-hot-toast'
 import Footer from './components/Footer'
+import Layout from './pages/admin/layout'
+import Dashboard from './pages/admin/Dashboard'
+import ListBookings from './pages/admin/ListBookings'
+import ListShows from './pages/admin/ListShows'
+import AddShows from './pages/admin/AddShows'
 
 
 const App = () => {
@@ -16,17 +21,23 @@ const App = () => {
   const isAdminRoute = useLocation().pathname.startsWith('/admin')
   return (
     <>
-      <Toaster/>
-      {!isAdminRoute &&<Navbar/>}
+      <Toaster />
+      {!isAdminRoute && <Navbar />}
       <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/movies' element={<Movies/>} />
-        <Route path='/movies/:id' element={<MovieDetails/>} />
-        <Route path='/movies/:id/:date' element={<SeatLayout/>} />
-        <Route path='/my-bookings' element={<MyBookings/>} />
-        <Route path='/favourite' element={<Favourite/>} />
+        <Route path='/' element={<Home />} />
+        <Route path='/movies' element={<Movies />} />
+        <Route path='/movies/:id' element={<MovieDetails />} />
+        <Route path='/movies/:id/:date' element={<SeatLayout />} />
+        <Route path='/my-bookings' element={<MyBookings />} />
+        <Route path='/favourite' element={<Favourite />} />
+        <Route path='/admin/*' element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path='add-shows' element={<AddShows />} />
+          <Route path='list-shows' element={<ListShows />} />
+          <Route path='list-bookings' element={<ListBookings />} />
+        </Route>
       </Routes>
-      {!isAdminRoute && <Footer/>}
+      {!isAdminRoute && <Footer />}
     </>
   )
 }
